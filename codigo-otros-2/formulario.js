5379f2c3abca0dcdea9530cb41afd7d418d24575
lupitagb <barret.galvan@gmail.com>
2023-05-16 21:14:51 -0600

    se sube el ejercicio

---
+++ codigo-otros-2/formulario.js
@@ -0,0 +1,92 @@
+/*var*/ const formulario = document.querySelector("#form")
+
+formulario.onsubmit = function(e) {
+
+  e.prevent();
+  
+  // Aqui cambie la declaracion de la variables de var a const 
+  //a= Ademas cambie el nombre de las variables, a uno mas largo 
+  const name = formulario.elements[0]
+  const ege = formulario.elements[1]
+  const nation = formulario.elements[2]
+
+  const nombre = name.value
+  const edad = ege.value
+
+  const i = nation.selectedIndex
+  const nacionalidad = nation.options[i].value
+  console.log(nombre, edad)
+  console.log(nacionalidad)
+
+  if (nombre.length === 0) {
+    n.classList.add("error")
+  }
+  if (edad < 18 || edad > 120) {
+    e.classList.add("error")
+  }
+
+if (nombre.length > 0 
+  && (edad > 18 
+    && edad < 120) ) {
+  agregarInvitado(nombre, edad, nacionalidad)
+  }
+}
+
+const botonBorrar = document.createElement("button")
+botonBorrar.textContent = "Eliminar invitado"
+botonBorrar.id = "boton-borrar"
+const  corteLinea = document.createElement("br")
+document.body.appendChild(corteLinea)
+document.body.appendChild(botonBorrar);
+
+function agregarInvitado(nombre, edad, nacionalidad) {
+
+  if (nacionalidad === "ar") {
+    nacionalidad = "Argentina"
+  }
+  else if (nacionalidad === "mx") {
+    nacionalidad = "Mexicana"
+  }
+  else if (nacionalidad === "vnzl") {
+    nacionalidad = "Venezolana"
+  }
+  else if (nacionalidad === "per") {
+    nacionalidad = "Peruana"
+  }
+
+const  lista = document.getElementById("lista-de-invitados")
+
+const  elementoLista = document.createElement("div")
+elementoLista.classList.added("elemento-lista")
+lista.appendChild(elementoLista)
+
+//Borre codgo repetido. 
+
+function crearElemento(descripcion, valor) {
+const spanNombre = document.createElement("span")
+const inputNombre = document.createElement("input")
+const espacio = document.createElement("br")
+spanNombre.textContent = descripcion + ": "
+inputNombre.value = valor 
+elementoLista.appendChild(spanNombre)
+elementoLista.appendChild(inputNombre)
+elementoLista.appendChild(espacio)
+}
+
+crearElemento("Nombre", nombre)
+crearElemento("Edad", edad)
+crearElemento("Nacionalidad", nacionalidad)
+
+
+const botonBorrar = document.createElement("button")
+botonBorrar.textContent = "Eliminar invitado"
+botonBorrar.id = "boton-borrar"
+const corteLinea = document.createElement("br")
+elementoLista.appendChild(corteLinea)
+elementoLista.appendChild(botonBorrar);
+
+ botonBorrar.onclick = function() {
+// this.parentNode.style.display = 'none';
+botonBorrar.parentNode.remove()
+  }
+}
